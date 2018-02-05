d037fff4b228ab6e6b2de137107edcba0c92807a
gluxix <gluxix1993@gmail.com>
2018-02-05 23:22:39 +0300

    Refactoring

---
+++ handlers.js
@@ -0,0 +1,90 @@
+const sch = require('./schedule');
+const msgs = require('./messages');
+
+module.exports = function(ctx) {
+  let module = {};
+
+  const { bot, redis, logger } = ctx;
+
+  module.rings = function(msg, match) {
+    sch.rings().then(times => {
+      bot.sendMessage(msg.chat.id, `Звонки:\n${times.join("\n")}`);
+    }).catch(err => {
+      logger.error(err);
+      bot.sendMessage(msg.chat.id, msgs.error);
+    });
+  };
+
+  module.memorizeGroup = function(msg, match) {
+    const groupName = match[1];
+
+    if (!groupName) {
+      bot.sendMessage(msgs.group.forgotName);
+      return;
+    }
+
+    sch.detectGroup(groupName)
+      .then(groups => {
+        if (groups.length == 0) {
+          bot.sendMessage(msg.chat.id, msgs.group.notFound);
+        } else if (groups.length > 1) {
+          bot.sendMessage(msg.chat.id, `Список похожих групп:\n${groups.map(g => g.name).join("\n")}`);
+        } else {
+          redis.set(msg.from.id, groups[0].id);
+          bot.sendMessage(msg.chat.id, `Я запомнил, что вы учитесь в группе ${groups[0].name} ;)`);
+        }
+      })
+      .catch(err => {
+        logger.error(err);
+        bot.sendMessage(msg.chat.id, msgs.error);
+      });
+  };
+
+  module.link = function(msg, match) {
+    redis.get(msg.from.id).then(groupId => {
+      if (!groupId) throw 'Group id not found';
+      bot.sendMessage(msg.chat.id, `https://vyatsuschedule.herokuapp.com/mobile/${groupId}/${process.env.SEASON}`);
+    }).catch(err => {
+      logger.error(err);
+      bot.sendMessage(msg.chat.id, msgs.forgotStudent);
+    });
+  };
+
+  module.schedule = function(msg, match, nextDay) {
+    redis.get(msg.from.id)
+      .then(groupId => {
+        if (!groupId) throw 'Group id not found';
+        return Promise.all([sch.rings(true), sch.schedule(groupId, nextDay)]);
+      })
+      .then(values => {
+        const rings = values[0];
+        const schedule = values[1];
+        answer = [];
+        rings.forEach((v, i) => {
+          if (schedule.day[i]) {
+            answer.push(`${v} > ${schedule.day[i]}`);
+          }
+        });
+        let options = {};
+        if (!nextDay) {
+          options.reply_markup = { 
+            inline_keyboard: [
+              [
+                { text: 'Next', callback_data: { type: 'next', groupId } }
+              ]
+            ]
+          };
+        }
+        bot.sendMessage(
+          msg.chat.id,
+          `Расписание (${schedule.date.toLocaleDateString()}, ${dateHelper.dayName(schedule.date)}):\n${answer.join("\n")}`,
+          options
+        );
+      })
+      .catch(err => {
+        bot.sendMessage(msg.chat.id, msgs.forgotStudent);
+      });
+  };
+
+  return module;
+};
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const URL = process.env.URL;
 
 const sch = require('./schedule');
 const dateHelper = require('./helpers/date');
+const msgs = require('./messages');
+
+const Promise = require("bluebird");
 
 // Logger
 const logger = require('log4js').getLogger();
@@ -17,6 +20,8 @@ const Visit = require('./models/Visit');
 
 // Redis
 const redis = require('redis').createClient(process.env.REDIS_URL);
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
 redis.on("error", function (err) {
   logger.error("Error " + err);
 });
@@ -30,16 +35,12 @@ const bot = new TelegramBot(TOKEN, {
 });
 bot.setWebHook(`${URL}${TOKEN}`);
 
-const HELP =
-`Чат бот ВятГУ для просмотра расписания студентов. Альфа-бета-гамма версия. Могут быть баги.
-Команды (палка "|" означает ИЛИ):
-1. з|звонки|r|rings - расписание звонков
-2. н|неделя|w|week - номер текущей недели
-3. г|группа|g|group имя_группы - бот запомнит, в какой вы группе (можно вводить не полностью, предложит варианты)
-4. р|расписание|s|schedule - расписание на текущий день (работает, если бот знает группу)
-5. u|url|с|ссылка - ссылка на полное расписание группы
-* Я пока молодой бот, меня батька создал лежа на диване сегодня с утра (4 февраля 2018), поэтому пишите ему https://vk.com/volodyaglyxix`;
+// Save all needed dependencies to ctx
+const ctx = { logger, redis, bot }
 
+const { rings, memorizeGroup, link, schedule } = require('./handlers')(ctx);
+
+// Save request
 bot.on('message', (msg) => {
   const visit = new Visit({ telegram_id: msg.from.id, message: msg.text });
   visit.save();
@@ -48,24 +49,17 @@ bot.on('message', (msg) => {
 
 // Start bot
 bot.onText(/\/start/, (msg, match) => {
-  bot.sendMessage(msg.chat.id, 'Чат бот ВятГУ для просмотра расписания студентов. Для справки введите help или помощь.');
+  bot.sendMessage(msg.chat.id, msgs.desc);
 });
 
 // Help
 bot.onText(/^\/?(help|помощь)$/i, (msg, match) => {
-  bot.sendMessage(msg.chat.id, HELP);
+  bot.sendMessage(msg.chat.id, msgs.help);
 });
 
 // Rings
 bot.onText(/^\/?(r|rings|з|звонки)$/i, (msg, match) => {
-  sch.rings()
-    .then(rings => {
-      bot.sendMessage(msg.chat.id, `Звонки:\n${rings.join("\n")}`);
-    })
-    .catch(err => {
-      logger.error(err);
-      bot.sendMessage(msg.chat.id, 'Ууупс... Какая-то ошибка :(');
-    });
+  rings(msg, match);
 });
 
 // Week
@@ -74,109 +68,25 @@ bot.onText(/^\/?(w|week|н|неделя)$/i, (msg, match) => {
 });
 
 // Memorize group
-bot.onText(/^\/?(g|group|г|группа)(.+)?$/i, (msg, match) => {
-  if (match[2]) {
-    sch.detectGroup(match[2])
-      .then(res => {
-        if (res.length == 0) {
-          bot.sendMessage(msg.chat.id, 'Подходящих групп не найдено. Возможно Вы вводите группу с ошибкой, пример: ИВТб-3302-02-00 (без учета регистра)');
-        } else if (res.length > 1) {
-          bot.sendMessage(msg.chat.id, `Список похожих групп:\n${res.map(g => g.name).join("\n")}`);
-        } else {
-          redis.set(msg.from.id, res[0].id);
-          bot.sendMessage(msg.chat.id, `Я запомнил, что вы учитесь в группе ${res[0].name} ;)`);
-        }
-      })
-      .catch(err => {
-        logger.error(err);
-        bot.sendMessage(msg.chat.id, 'Ууупс... Какая-то ошибка :(');
-      });
-  } else {
-    bot.sendMessage(msg.chat.id, 'Повторите сообщение, не забыв указать имя группы :)');
-  }
+bot.onText(/^\/?g|group|г|группа(.+)$/i, (msg, match) => {
+  memorizeGroup(msg, match);
 });
 
 // Schedule url
 bot.onText(/^(url|u|ссылка|с)$/i, (msg, match) => {
-  redis.get(msg.from.id, (err, groupId) => {
-    if (err) {
-      logger.error(`Error: ${err}`);
-      return;
-    }
-
-    if (groupId) {
-      bot.sendMessage(msg.chat.id, `https://vyatsuschedule.herokuapp.com/mobile/${groupId}/${process.env.SEASON}`);
-    } else {
-      bot.sendMessage(msg.chat.id, "К сожалению, я очень забывчивый бот. Либо Вы впервые здесь, либо я Вас забыл. Укажите группу, например:\n группа ивтб-3302");
-    }
-  });
+  link(msg, match);
 });
 
 // Schedule
 bot.onText(/^\/?(s|schedule|р|расписание)$/i, (msg, match) => {
-  redis.get(msg.from.id, (err, groupId) => {
-    if (err) {
-      logger.error(`Error: ${err}`);
-      return;
-    }
-
-    if (groupId) {
-      sch.schedule(groupId)
-        .then(schedule => {
-          sch.rings(true).then(rings => {
-            answer = [];
-            rings.forEach((v, i) => {
-              if (schedule.day[i]) {
-                answer.push(`${v} > ${schedule.day[i]}`);
-              }
-            });
-            const keyboard = {
-              inline_keyboard: [
-                [{ text: 'Next', callback_data: groupId }]
-              ]
-            };
-            bot.sendMessage(
-              msg.chat.id,
-              `Расписание (${schedule.date.toLocaleDateString()}, ${dateHelper.dayName(schedule.date)}):\n${answer.join("\n")}`,
-              { reply_markup: keyboard }
-            );
-          });
-        })
-        .catch(err => {
-          logger.error(err);
-          bot.sendMessage(msg.chat.id, 'Ууупс... Какая-то ошибка :(');
-        });
-    } else {
-      bot.sendMessage(msg.chat.id, "К сожалению, я очень забывчивый бот. Либо Вы впервые здесь, либо я Вас забыл. Укажите группу, например:\n группа ивтб-3302");
-    }
-  });
-});
-
-bot.onText(/test/i, (msg, match) => {
-  const keyboard = {
-    inline_keyboard: [
-      [{ text: 'Next', callback_data: 'next' }]
-    ]
-  };
-  bot.sendMessage(msg.chat.id, 'text', { reply_markup: keyboard });
+  schedule(msg, match);
 });
 
 bot.on('callback_query', function (msg) {
   message = msg.message;
-  sch.schedule(msg.data, 1)
-    .then(schedule => {
-      sch.rings(true).then(rings => {
-        answer = [];
-        rings.forEach((v, i) => {
-          if (schedule.day[i]) {
-            answer.push(`${v} > ${schedule.day[i]}`);
-          }
-        });
-        bot.sendMessage(message.chat.id, `Расписание (${schedule.date.toLocaleDateString()}, ${dateHelper.dayName(schedule.date)}):\n${answer.join("\n")}`);
-      });
-    })
-    .catch(err => {
-      logger.error(err);
-      bot.sendMessage(message.chat.id, 'Ууупс... Какая-то ошибка :(');
-    });
+  switch(msg.data.type) {
+    case 'next':
+      schedule(message, null, true);
+      break;
+  }
 });
