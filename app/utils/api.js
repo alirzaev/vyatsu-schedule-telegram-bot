79c6e721cc98b71002e485b71fafe97edd8720af
Ali Rzaev <alirzaev1996@gmail.com>
2018-07-14 12:43:08 +0300

    Implement module for interacting with API server
    This module was created to simplify and standardize interaction with API server. Also moving API calls to the single module simplifies unit tests writing.

---
+++ app/utils/api.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+
+const BASE_API = process.env.BASE_API;
+
+module.exports = {
+
+    season: async () => {
+        const resp = axios.get(`${BASE_API}/v2/season/current`);
+        const data = resp['data'];
+
+        if (data['error']) {
+            return null;
+        } else {
+            return data['season'];
+        }
+    },
+
+    schedule: async (groupId, season) => {
+        const resp = await axios.get(`${BASE_API}/v2/schedule/${groupId}/${season}`);
+        const data = resp['data'];
+
+        if (data['error']) {
+            return null;
+        } else {
+            return data;
+        }
+    },
+
+    groups: async () => {
+        const resp = await axios.get(`${BASE_API}/v2/groups/by_faculty`);
+
+        const data = resp['data'];
+
+        if (data['error']) {
+            return null;
+        } else {
+            return data;
+        }
+    }
+
+};
