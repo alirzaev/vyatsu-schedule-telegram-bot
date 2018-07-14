53fd6b36f31dc79f3e120b5ac4c4caab0d095ffd
Ali Rzaev <alirzaev1996@gmail.com>
2018-07-14 12:28:13 +0300

    Implement functions for beautifying text information
    This functions are intended for formatting following text data in messages that bot sends to user:
    
    - Schedule lessons. Reducing names of the lessons types, removing group's name from the lesson's description
    
    - Rings schedule. Transform each ring range into the following string: '<number of range>) <range's begin>-<range's end>'
    
    - Schedule. Combine each pair of lesson and ring range into one string

---
+++ app/utils/beautify.js
@@ -0,0 +1,30 @@
+function zip(...arrays) {
+    const minLength = Math.min(...(arrays.map(array => array.length)));
+
+    const iterable = {};
+    iterable[Symbol.iterator] = function* () {
+        for (let i = 0; i < minLength; ++i) {
+            const items = arrays.map(array => array[i]);
+
+            yield items;
+        }
+    };
+
+    return Array.of(...iterable);
+}
+
+module.exports = {
+
+    lessons: (lessons) => lessons.map(lesson => lesson
+        .replace('\r', ' ')
+        .replace(/Чтение ?лекций/im, 'Лек.')
+        .replace(/Проведение ?лабораторных ?занятий/im, 'Лаб.')
+        .replace(/Проведение ?практических ?занятий,? ?семинаров/im, 'Пр.')
+        .replace(/(.+-\d{4}-\d{2}-\d{2}, )/im, '') // remove the group name from the lesson's description
+    ),
+
+    rings: (rings) => rings.map((rings, index) => `${index + 1}) ${rings.start}-${rings.end}`),
+
+    schedule: (rings, lessons) => zip(rings, lessons).map(v => `*${v[0].start} >* ${v[1]}`)
+
+};
