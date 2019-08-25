c2abc0b9e5548041790951b9d9bcaa1b798e1c29
AliRzaev <alirzaev1996@gmail.com>
2019-08-25 16:58:43 +0300

    Code refactoring
    - Completely move bot initialization into initialize() function
    
    - Add function start() that runs bot
    
    - Update log messages
    
    - Remove field 'main' from package.json
    
    - Remove unused dependencies

--- src/configs/bot.js
+++ src/configs/bot.js
@@ -4,30 +4,43 @@ const TOKEN = process.env.TOKEN;
 const PORT = process.env.PORT;
 const WEBHOOK_URL = process.env.WEBHOOK_URL;
 
-const options = {};
-
-if (WEBHOOK_URL) {
-    options.webHook = {
-        port: PORT || 80
-    };
-} else {
-    options.polling = {
-        params: {
-            timeout: 10
-        }
-    };
-}
-
-const bot = new TelegramBot(TOKEN, options);
+let bot = null;
 
 module.exports = {
     initialize: async () => {
+        const options = {};
         if (WEBHOOK_URL) {
-            await bot.setWebHook(`${WEBHOOK_URL}${TOKEN}`);
+            options.webHook = {
+                port: PORT || 80,
+                autoOpen: false
+            };
+        } else {
+            options.polling = {
+                autoStart: false,
+                params: {
+                    timeout: 10
+                }
+            };
         }
+
+        bot = new TelegramBot(TOKEN, options);
     },
 
     instance: () => {
-        return bot;
+        if (bot) {
+            return bot;
+        } else {
+            throw new Error('Bot is not initialized');
+        }
+    },
+
+    start: async () => {
+        if (WEBHOOK_URL) {
+            await bot.openWebHook();
+            const normalizedUrl = WEBHOOK_URL.endsWith('/') ? WEBHOOK_URL : WEBHOOK_URL + '/';
+            await bot.setWebHook(`${normalizedUrl}${TOKEN}`);
+        } else {
+            await bot.startPolling();
+        }
     }
 };
